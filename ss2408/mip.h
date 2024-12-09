0dbb82e1dd7ba54f7f660ad801e4281d74012c58
{hayashi} <{chihohaya4@gmail.com}>
2024-12-10 01:17:05 +0900

    [add]階調変換まで完了

--- ss2408/mip.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include "euler_angles.h"
-
-std::vector<unsigned char> generate_mip_image(
-    const std::vector<unsigned char>& raw_data,
-    int width, int height, int depth,
-    const EulerAngles& angles = EulerAngles());
